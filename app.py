c8282e1917873aa59558eb6ec43865a975f68738
nkw5772 <nkw5772@uncw.edu>
2024-11-07 16:35:18 -0500

    Added cookies (Yum!)
    Added support for cookies
    Creating a cookie when logging into an account and deleting that cookie when logging out
    Update home.html to include a logout button
    Added app.route('/logout')

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, make_response
 from hashlib import sha256
 
 app = Flask(__name__)
@@ -7,7 +7,49 @@ app = Flask(__name__)
 # Define the route for the login page
 @app.route('/')
 def index():
+<<<<<<< Updated upstream
     return render_template('home.html')
+=======
+    if request.method == 'POST':
+        db = helper.Database()
+        # Get the form data
+        username = request.form.get('username')
+        # password = request.form.get('password')
+        password_hash = sha256(request.form.get('password').encode('utf-8')).hexdigest()
+        
+        # Check if the credentials are correct
+        if db.verify_login(username, password_hash) == True: 
+            resp = make_response(redirect(url_for('home')))
+            resp.set_cookie('auth_token', username)
+            return resp
+            # return render_template('home.html')
+        else:
+            # Show an error message if credentials are incorrect
+            # return redirect(url_for('error'))
+            # return redirect(url_for('index'))
+            error_message = "Invalid username or password. Please try again."
+            return render_template('login.html', error_message=error_message)
+
+    # If it's a GET request, render the login page
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    resp = make_response(redirect(url_for('createAccount')))
+
+    resp.delete_cookie('auth_token')
+
+    return resp
+
+@app.route("/home")
+def home():
+    username = request.cookies.get('auth_token')
+    if username:
+        return render_template('home.html')
+    else:
+        return "You are not logged in."
+    
+>>>>>>> Stashed changes
 
 @app.route('/login')
 def login():
@@ -15,6 +57,40 @@ def login():
 
 @app.route("/createAccount")
 def createAccount():
+<<<<<<< Updated upstream
+=======
+    if request.method == 'POST':
+        
+        db = helper.Database()
+        
+        first_name = request.form.get('first_name') # Should we hash this info as well?
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        phone_number = request.form.get('phone')
+        password_hash = sha256(request.form.get('password').encode('utf-8')).hexdigest()
+        confirm_password_hash = sha256(request.form.get('confirm_password').encode('utf-8')).hexdigest()
+
+        error_messages = []
+
+        if not db.email_is_unique(email):
+            error_messages.append('Email is already in use - please use a different email.')
+
+        if not db.phone_is_unique(phone_number):
+            error_messages.append('Phone number is already in use - please use a different phone number.')
+
+        if password_hash != confirm_password_hash:
+            error_messages.append('Passwords do not match.')
+        
+        if error_messages:
+            return render_template('createAccount.html', error_messages=error_messages)
+        else:
+            today = datetime.today().strftime('%Y-%m-%d')
+            db.add_account(first_name, last_name, 'Customer', email, phone_number, today, password_hash)
+            return render_template('home.html', message=f"Welcome {first_name}!") # I wanna be able to redirect with parameters (haven't researched much yet)
+            # return redirect(url_for('home'))
+
+    # If it's a GET request, render the login page
+>>>>>>> Stashed changes
     return render_template('createAccount.html')
 
 @app.route("/createReservation")
